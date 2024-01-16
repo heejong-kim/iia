739fe37d3614d9318cbb7bdc03aeac63cd07b36e
heejong-kim <heejong.hj.kim@gmail.com>
2024-01-15 23:58:32 -0500

    iia visualization

--- cnn_saliency_map_generator.py
+++ cnn_saliency_map_generator.py
@@ -18,10 +18,10 @@ from salieny_models import *
 from saliency_lib import *
 from torchvision.datasets import VOCDetection
 from torchvision.datasets import CocoDetection
-from torchgc.pytorch_grad_cam.fullgrad_cam import FullGrad
-from torchgc.pytorch_grad_cam.layer_cam import LayerCAM
-from torchgc.pytorch_grad_cam.ablation_cam import AblationCAM
-from torchgc.pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.fullgrad_cam import FullGrad
+from pytorch_grad_cam.layer_cam import LayerCAM
+from pytorch_grad_cam.ablation_cam import AblationCAM
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 
 IMAGE_SIZE = 'image_size'
 
@@ -34,7 +34,7 @@ IS_VOC_BBOX = False
 IS_COCO_BBOX = False
 INPUT_SCORE = 'score_original_image'
 IMAGE_PIXELS_COUNT = 50176
-INTERPOLATION_STEPS = 10
+INTERPOLATION_STEPS = 10 # TODO : default is 10 based on the paper
 # INTERPOLATION_STEPS = 3
 LABEL = 'label'
 TOP_K_PERCENTAGE = 0.25
@@ -46,6 +46,156 @@ DATA_VAL_TXT = 'data/val.txt'
 # DATA_VAL_TXT = f'data/pics.txt'
 
 device = 'cuda'
+
+from model import *
+
+
+def create_sequential_model_without_top(model, num_top_layers=1):
+    model_children_without_top_layers = list(model.children())[:-num_top_layers]
+    return nn.Sequential(*model_children_without_top_layers)
+
+test = create_sequential_model_without_top(model)
+
+class GradModel(nn.Module):
+    def __init__(self, model_name='resnet50', feature_layer=8):
+        super(GradModel, self).__init__()
+        self.post_features = None
+        self.model_str = 'None'
+
+        if model_name == 'pairnet':
+            model = CNN2DsmallFeatureDiff(inputsize=224, channels=1,
+                                          initial_channel=16,
+                                          n_of_blocks=4, num_conv=1, pooling=nn.AvgPool2d)
+
+            self.features = model.encoder
+            self.post_features = None
+            self.avgpool = None
+            self.classifier = model.linear
+
+
+        elif model_name == 'resnet101':
+            model = create_resnet101_module(requires_grad=True, pretrained=True)
+            self.features = model[:feature_layer]
+            self.post_features = model[feature_layer:-1]
+            self.avgpool = model[-1:]
+            self.classifier = torchvision.models.resnet101(pretrained=True).fc
+        elif model_name == 'resnet50':
+            model = create_resnet50_module(requires_grad=True, pretrained=True)
+            self.features = model[:feature_layer]
+            self.post_features = model[feature_layer:-1]
+            self.avgpool = model[feature_layer:]
+            self.classifier = torchvision.models.resnet50(pretrained=True).fc
+        elif model_name == 'resnet18':
+            model = create_resnet18_module(requires_grad=True, pretrained=True)
+            self.features = model[:feature_layer]
+            self.post_features = model[feature_layer:-1]
+            self.avgpool = model[-1:]
+            self.classifier = torchvision.models.resnet18(pretrained=True).fc
+        elif model_name == 'densnet':
+            model = torchvision.models.densenet201(pretrained=True)
+            model.eval()
+            self.features = model.features[:feature_layer]
+            self.post_features = model.features[feature_layer:-1]
+            self.avgpool = torch.nn.AvgPool2d(kernel_size=7, stride=1)
+            self.classifier = model.classifier
+        # elif model_name == 'convnext':
+        #     self.model_str = model_name
+        #     model = torchvision.models.convnext_base(weights=ConvNeXt_Base_Weights.IMAGENET1K_V1)
+        #     self.features = model.features
+        #     self.post_features = model.features[feature_layer:-1]
+        #     self.avgpool = model.avgpool
+        #     self.classifier = model.classifier
+        elif model_name == 'vgg16':
+            model = torchvision.models.vgg16(pretrained=True)
+            self.features = model.features
+            self.avgpool = model.avgpool
+            self.classifier = model.classifier
+        elif model_name == 'vgg19':
+            model = torchvision.models.vgg19(pretrained=True)
+            self.features = model.features
+            self.avgpool = model.avgpool
+            self.classifier = model.classifier
+        else:
+            raise NotImplementedError('No such model')
+
+        self.gradients = None
+
+    def activations_hook(self, grad):
+        self.gradients = grad.detach().clone()
+
+    def activations_hook_t(self, grad):
+        self.gradients_t = grad.detach().clone()
+
+    def activations_hook_s(self, grad):
+        self.gradients_s = grad.detach().clone()
+
+    def get_activations_gradient(self):
+        return self.gradients
+
+    def get_activations_gradient_t(self):
+        return self.gradients_t
+
+    def get_activations_gradient_s(self):
+        return self.gradients_s
+
+    def get_activations(self, x):
+        x = self.features(x)
+        return x
+
+    def get_post_activations(self, x):
+        x = self.post_features(x)
+        return x
+
+    def get_post_activations_middle(self, x):
+        x = self.post_features[:-1](x)
+        return x
+
+    def compute_representation(self, x, hook=True):
+        x = self.forward_(x, hook)
+        return x
+
+    def compute_t_s_representation(self, t, s, only_post_features=False):
+        t = self.forward_(t, True, only_post_features, hook_func=lambda grad: self.activations_hook_t(grad))
+        s = self.forward_(s, True, only_post_features, hook_func=lambda grad: self.activations_hook_s(grad))
+
+        return t, s
+
+    def activations_to_features(self, x):
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        return x
+
+    def forward_(self, x, hook=True, only_post_features=False, hook_func=None, is_middle=False):
+        if len(list(x.shape)) > 4:
+            x = x.squeeze(1)
+
+        if not only_post_features:
+            x = self.features(x)
+
+        if hook:
+            hook_func = self.activations_hook if hook_func is None else hook_func
+            x.register_hook(hook_func)
+
+        if self.post_features is not None:
+            if is_middle:
+                x = self.post_features[:-1](x)
+            else:
+                x = self.post_features(x)
+
+
+        if self.model_str == 'convnext':
+            x = F.relu(x)
+            x = self.avgpool(x)
+        else:
+            x = F.relu(x)
+            x = self.avgpool(x)
+            x = torch.flatten(x, 1)
+        return x
+
+    def forward(self, x, hook=True, only_post_features=False, is_middle=False):
+        x = self.forward_(x, hook=hook, only_post_features=only_post_features, is_middle=is_middle)
+        x = self.classifier(x)
+        return x
 
 
 def get_grads_wrt_image(model, label, images_batch, device='cuda', steps=50):
@@ -363,7 +513,7 @@ def heatmap_of_layer(device, image_path, interpolation_on_activations_steps_arr,
     igacts = get_interpolated_values(basel.detach(), activations_featmap_list,
                                      interpolation_on_activations_steps_arr[-1]).detach()
     grads = []
-    for act in igacts:
+    for act in igacts: # TODO Activation stage layer gradient wrt prediction
         act.requires_grad = True
         grads.append(calc_grads_model(model, (act.squeeze()), device, label).detach())
         act = act.detach()
@@ -659,7 +809,7 @@ def by_class_map(model, image, label, operation='ours', use_mask=False):
     gradients = model.get_activations_gradient()
     heatmap = grad2heatmaps(model, image.unsqueeze(0).to(device), gradients, activations=None, operation=operation,
                             score=score, do_nrm_rsz=True,
-                            weight_ratio=weight_ratio)
+                            weight_ratio=weight_ratio) # regular heatmap?
 
     t = tensor2cv(image)
     blended_img, score, heatmap_cv, blended_img_mask, img_cv = blend_image_and_heatmap(t, heatmap, use_mask=use_mask)
